1ebc8cec13ffccf87326848dfa3c73f5a790b598
Michal Harish <michal.haris@imagini.net>
2014-04-01 15:49:57 +0100

    Updated README and correct dependency versions

--- src/test/scala/co/gridport/scala/EchoServiceTest.scala
+++ src/test/scala/co/gridport/scala/EchoServiceTest.scala
@@ -3,11 +3,11 @@ package co.gridport.scala
 import org.scalatest._
 import co.gridport.scala._
 
-class EchoServiceTest extends FlatSpec with Matchers {
+class EchoServiceTest extends FlatSpec  {
 
   info("Example test with FlatSpec")
 
   "EchoService echo hello" should "return hello" in {
-    EchoService("hello") should equal("hello")
+    assert(EchoService("hello") === "hello")
   }
 }
